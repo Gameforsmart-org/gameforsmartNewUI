c5131d7cc125da75047d94659fa81bddb06c7ae0
gameforsmart <gameforsmartubig@gmail.com>
2026-03-11 08:13:15 +0700

    Update page.tsx

--- app/(auth)/create/page.tsx
+++ app/(auth)/create/page.tsx
@@ -4,8 +4,17 @@
 // Minimal entry – logic is in _hooks/useCreateQuiz.ts
 // UI is in _components/CreateQuizLayout.tsx
 // ============================================================
-
 import { CreateQuizLayout } from "./component/CreateQuizLayout";
+import { generateMeta } from "@/lib/utils";
+
+export async function generateMetadata() {
+  return generateMeta({
+    title: "Create Quiz",
+    description:
+      "The admin dashboard template offers a sleek and efficient interface for monitoring important data and user interactions. Built with shadcn/ui.",
+    canonical: "/create"
+  });
+}
 
 export default function CreateQuizPage() {
   return <CreateQuizLayout />;
